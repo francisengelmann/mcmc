c3224010c97cd300a68f3034b79661421a02a4a6
Francis Engelmann <francis.engelmann@gmail.com>
2015-10-12 20:03:48 +0200

    Updated code to work with opencv3

--- src/sudoku.cpp
+++ src/sudoku.cpp
@@ -6,8 +6,9 @@
 //
 
 // OpenCV includes
-#include <opencv2/core/core.hpp>
-#include <opencv2/highgui/highgui.hpp>
+#include "opencv2/core/core.hpp"
+#include "opencv2/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
 
 // C/C++ includes
 #include <iostream>
